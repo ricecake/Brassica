6c9b27cf810da614b4cc862e5be9eb010b5d2a4d
ricecake <ricecake@greenstuff.io>
2026-09-18 23:24:08 -0400

    Define verbose potential phases

--- include/passes/RenderPhases.hpp
+++ include/passes/RenderPhases.hpp
@@ -17,7 +17,24 @@
 
 namespace brassica {
 	namespace SubPhase {
+		inline constexpr graph::Phase Prepare = graph::Phase(-500);
+		inline constexpr graph::Phase GBuffer = graph::Phase(0);
+		inline constexpr graph::Phase LightPreparation = graph::Phase(500); //Light Clustering & Froxel Volumetric Injection
+		inline constexpr graph::Phase GIComput = graph::Phase(600);
+		inline constexpr graph::Phase DeferredShading = graph::Phase(700);//Deferred Direct Lighting (Reads G-Buffer, Lights, GI, and Froxels)
+		inline constexpr graph::Phase Reflection = graph::Phase(800); //SSR Compute & Composite (Reads G-Buffer, HZB, and Lit HDR)
+		inline constexpr graph::Phase Atmosphere = graph::Phase(900);//Distant Volumetrics & Atmosphere Composite
+		inline constexpr graph::Phase UnderwaterStructuralTranslucentRender = graph::Phase(1000);
+		inline constexpr graph::Phase UnderwaterParticleRender = graph::Phase(1100);
+		inline constexpr graph::Phase WaterRender = graph::Phase(1200);
+		inline constexpr graph::Phase StructuralTranslucent = graph::Phase(1300);
+		inline constexpr graph::Phase ParticleRender = graph::Phase(1400);
+		inline constexpr graph::Phase Exposure = graph::Phase(1500);
+		inline constexpr graph::Phase BloomAndOptics = graph::Phase(1600);
+		inline constexpr graph::Phase ToneMapping = graph::Phase(1700);
+		inline constexpr graph::Phase PostProcessing = graph::Phase(1800);
 		inline constexpr graph::Phase HudOverlay = graph::Phase(2000);
 		inline constexpr graph::Phase MenuOverlay = graph::Phase(3000);
+
 	} // namespace SubPhase
 } // namespace brassica
